115e8fa4e2bee76f2c5ab5ce4f5fbfaeba354eee
ValdirCastaldelliJr <valdir.castaldelli@gmail.com>
2024-03-15 15:46:24 +0100

    cleaning favorite link on delete

--- convex/board.ts
+++ convex/board.ts
@@ -51,7 +51,20 @@ export const remove = mutation({
         if (!identity) {
             throw new Error("Unauthorized");
         }
-        // TODO: Later check to delete favorite relation as well
+
+        const userId = identity.subject;
+
+        const existingFavorite = await ctx.db
+            .query("userFavorites")
+            .withIndex("by_user_board", (q) => q
+                .eq("userId", userId)
+                .eq("boardId", args.id)
+            )
+            .unique();
+
+        if (existingFavorite) {
+            await ctx.db.delete(existingFavorite._id);
+        }
 
         await ctx.db.delete(args.id);
     },
@@ -103,10 +116,9 @@ export const favorite = mutation({
 
         const existingFavorite = await ctx.db
             .query("userFavorites")
-            .withIndex("by_user_board_org", (q) => q
+            .withIndex("by_user_board", (q) => q
                 .eq("userId", userId)
                 .eq("boardId", board._id)
-                .eq("orgId", args.orgId)
             )
             .unique();
 
@@ -146,7 +158,6 @@ export const unfavorite = mutation({
             .withIndex("by_user_board", (q) => q
                 .eq("userId", userId)
                 .eq("boardId", board._id)
-                // TODO: check if the orgId needed
             )
             .unique();
 
